5bedc3298121ed417dea0c9c945663e98f87b596
EladioDilag <EladioDilag@fullsail.edu>
2015-10-24 05:16:18 -0400

    Revert "Revert "Items Added""
    This reverts commit f85d2e16ab1250f4d9ec9474c24587a50f8b951c.

---
+++ Horror Game Jam 2015/Assets/Scripts/Items/Item_Base.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Item_Base : MonoBehaviour {
+
+    public    int num_Uses  { get; set; }
+
+    protected int num_Slots { get; set; }
+    
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    // Use is called when an object is used
+    protected void Use () {
+        if (num_Uses > 0)
+            num_Uses--;
+    }
+}
