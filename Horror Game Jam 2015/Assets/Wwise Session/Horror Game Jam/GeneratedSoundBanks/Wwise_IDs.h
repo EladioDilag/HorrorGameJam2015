897bb904af6af720009632b18968013b14b29c6b
ksugimurasound <k.sugimurasound@gmail.com>
2015-10-23 22:00:01 -0400

    Wwise Creation Audio Wizardry
    Audio Wizardry

---
+++ Horror Game Jam 2015/Assets/Wwise Session/Horror Game Jam/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,114 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID DO_NOT_TOUCH = 1426279440U;
+        static const AkUniqueID PLAY_IG_AMBIENCES = 676781980U;
+    } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace INGAME
+        {
+            static const AkUniqueID GROUP = 984691642U;
+
+            namespace STATE
+            {
+                static const AkUniqueID STAGE_01 = 344719509U;
+                static const AkUniqueID STAGE_02 = 344719510U;
+                static const AkUniqueID STAGE_03 = 344719511U;
+            } // namespace STATE
+        } // namespace INGAME
+
+        namespace PLAYER_SLASHER_DISTANCE
+        {
+            static const AkUniqueID GROUP = 4277253109U;
+
+            namespace STATE
+            {
+                static const AkUniqueID CENTER_RADIUS = 2198967719U;
+                static const AkUniqueID OUTER_RADIUS = 2110573087U;
+            } // namespace STATE
+        } // namespace PLAYER_SLASHER_DISTANCE
+
+        namespace STAGE_02
+        {
+            static const AkUniqueID GROUP = 344719510U;
+
+            namespace STATE
+            {
+            } // namespace STATE
+        } // namespace STAGE_02
+
+    } // namespace STATES
+
+    namespace SWITCHES
+    {
+        namespace MATERIALS
+        {
+            static const AkUniqueID GROUP = 4050929301U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID CONCRETE = 841620460U;
+                static const AkUniqueID DIRT = 2195636714U;
+                static const AkUniqueID GRASS = 4248645337U;
+            } // namespace SWITCH
+        } // namespace MATERIALS
+
+        namespace STATE_OF_PLAYER
+        {
+            static const AkUniqueID GROUP = 4154134024U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID DANGER = 4174463524U;
+                static const AkUniqueID DEAD = 2044049779U;
+                static const AkUniqueID NOPED = 470759487U;
+                static const AkUniqueID START = 1281810935U;
+                static const AkUniqueID YOU_ARE_FUCKED = 1604791578U;
+            } // namespace SWITCH
+        } // namespace STATE_OF_PLAYER
+
+    } // namespace SWITCHES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID STAGE_02 = 344719510U;
+        static const AkUniqueID STAGE_03 = 344719511U;
+    } // namespace GAME_PARAMETERS
+
+    namespace TRIGGERS
+    {
+        static const AkUniqueID IN_GAME_AMB = 2175852284U;
+    } // namespace TRIGGERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MAIN = 3161908922U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MASTER_SECONDARY_BUS = 805203703U;
+        static const AkUniqueID MX_BUS = 3027868960U;
+        static const AkUniqueID SFX_BUS = 1502772432U;
+        static const AkUniqueID STG_BUS = 19465429U;
+    } // namespace BUSSES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
