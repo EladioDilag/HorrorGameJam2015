c58ee8972d8e794b39702bef01f485532d0554b3
VGMghost <vgmghost@gmail.com>
2015-10-24 04:31:45 -0400

    Stashing Code
    Stashing code to move to a new laptop - DON'T PULL THIS BAD SHIT

--- Horror Game Jam 2015/Assets/Scripts/CharacterController.cs
+++ Horror Game Jam 2015/Assets/Scripts/CharacterController.cs
@@ -2,16 +2,21 @@
 using System.Collections;
 
 public class CharacterController : MonoBehaviour {
-
     public float walkSpeed;
     public float sprintSpeed;
 
     private float currentSpeed;
     private Rigidbody2D body;
+    private Light playerLight;
+    private float playerX;
+    private float playerY;
 
     void Start() {
         body = GetComponent<Rigidbody2D>();
+        playerLight = GetComponent<Light>();
         currentSpeed = walkSpeed;
+        //playerX = body.position.x;
+        //playerY = body.position.y;
     }
 
 	// Update is called once per frame
@@ -29,7 +34,11 @@ public class CharacterController : MonoBehaviour {
             currentSpeed = walkSpeed;
         }
 
-        body.velocity = new Vector2(Mathf.Lerp(0, (horizontal), 1),
+        if (Input.GetKey(KeyCode.A))
+        {
+            playerLight.transform.position = new Vector3(playerLight.transform.position.x-1, playerLight.transform.position.y, playerLight.transform.position.z);
+        }
+            body.velocity = new Vector2(Mathf.Lerp(0, (horizontal), 1),
                                     Mathf.Lerp(0, (vertical), 1)).normalized * currentSpeed;
     }
 }
