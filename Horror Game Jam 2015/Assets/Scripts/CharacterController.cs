34369ec57beb7871c2ac97d611ae1853f6a7cd42
Francisco Carrera <fc1159@nyu.edu>
2015-10-24 01:49:17 -0400

    Added Basic Character Control

---
+++ Horror Game Jam 2015/Assets/Scripts/CharacterController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterController : MonoBehaviour {
+
+    public float walkSpeed;
+    public float sprintSpeed;
+
+    private float currentSpeed;
+    private Rigidbody2D body;
+
+    void Start() {
+        body = GetComponent<Rigidbody2D>();
+        currentSpeed = walkSpeed;
+    }
+
+
+	// Update is called once per frame
+	void Update () {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        // Switch between sprinting and walking speeds
+        if (Input.GetKey (KeyCode.LeftShift))
+        {
+            currentSpeed = sprintSpeed;
+        }
+        else
+        {
+            currentSpeed = walkSpeed;
+        }
+
+
+        body.velocity = new Vector2(Mathf.Lerp(0, (horizontal * currentSpeed), 1), 
+                                    Mathf.Lerp(0, (vertical * currentSpeed), 1));
+    }
+}
